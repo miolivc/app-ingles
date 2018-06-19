3810dea7e43459408d4fe27909fd334f86afad67
Michelle Oliveira <miolivc@outlook.com>
2018-06-19 17:06:40 -0300

    Model Frase e mock de frase

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,9 @@
+import { Frase } from "../shared/frase.model";
+
+const FRASES : Array<Frase> = [
+    { fraseEng : 'I like to learn!' , frasePtBr : 'Eu gosto de aprender!' },
+    { fraseEng : 'I watch TV.' , frasePtBr : 'Eu assisto TV.' },
+    { fraseEng : 'How are you?' , frasePtBr : 'Como vai você?' },
+    { fraseEng : 'I eat bread.' , frasePtBr : 'Eu como pão.' },
+    { fraseEng : 'How old are you?' , frasePtBr : 'Qual é a sua idade?' }
+];
