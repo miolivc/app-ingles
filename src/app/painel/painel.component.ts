d2ca490d816fb39dda536ad5397c8a0e532756ab
Michelle Oliveira <miolivc@outlook.com>
2018-06-19 17:19:08 -0300

    Export FRASES mock e adicionando instrucao no painel dinamico

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frases-mock';
+
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
@@ -7,7 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  instrucao : string = 'Traduza a frase:';
+  frases : Frase[] = FRASES;
+
+  constructor() { 
+    console.log(this.frases);
+  }
 
   ngOnInit() {
   }
